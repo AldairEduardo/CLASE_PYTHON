c236270155b4c144422117c81728e1e816ec1fb0
AldairEduardo <ahuamanir@autonoma.edu.pe>
2024-11-12 15:00:44 -0500

    Ejercicio Bucle

---
+++ semana3.py
@@ -0,0 +1,11 @@
+# Initialize a counter variable
+counter = 0
+
+# Set the condition for the loop
+while counter < 5:
+    # Code to be executed inside the loop
+    print("Counter:", counter)
+    
+    # Increment the counter
+    counter += 1
+    print("Hello, World!")
